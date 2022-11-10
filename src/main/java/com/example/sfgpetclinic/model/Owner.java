87c5a31210c0ba31b1818e0613ac6121a21ec2bc
Ankan <ankan.chanda74@gmail.com>
2022-11-10 19:23:31 +0530

    Adding POJOs, Closes #2

---
+++ src/main/java/com/example/sfgpetclinic/model/Owner.java
@@ -0,0 +1,8 @@
+package com.example.sfgpetclinic.model;
+
+/**
+ * @author ankanchanda
+ * @created 11/10/22
+ */
+public class Owner extends Person{
+}
---
+++ src/main/java/com/example/sfgpetclinic/model/Person.java
@@ -0,0 +1,26 @@
+package com.example.sfgpetclinic.model;
+
+/**
+ * @author ankanchanda
+ * @created 11/10/22
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
